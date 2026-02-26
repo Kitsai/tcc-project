f0b96db943b328476334eeef276be82a183d7880
Kitsai <lucas.rocha.3801@gmail.com>
2026-02-25 21:18:20 -0300

    initial-commit

---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss"],
+  ssr: false,
+  // devServer: {
+  //   host: "0",
+  // },
+  vite: {
+    clearScreen: false,
+    envPrefix: ["VITE_", "TAURI_"],
+    server: {
+      strictPort: true,
+    },
+  },
+  ignore: ["**/src-tauri/**"],
+});
