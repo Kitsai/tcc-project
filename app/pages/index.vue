fd9c9d03242f79393549eeebeb47f5332cc55bff
Kitsai <lucas.rocha.3801@gmail.com>
2026-03-15 13:09:48 -0300

    fix: fixed tauri detection on nuxt plugin

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,8 +2,8 @@
     <UContainer
         class="flex flex-col gap-4 justify-center items-center h-[80vh]"
     >
-        <UButton class="w-fit text-lg" label="Abrir Projeto" color="primary" />
-        <UButton class="w-fit text-lg" label="Criar Projeto" />
+        <UButton class="w-fit text-lg" label="Abrir Problema" color="primary" />
+        <UButton class="w-fit text-lg" label="Criar Problema" />
     </UContainer>
 </template>
 
--- app/plugins/tauri.ts
+++ app/plugins/tauri.ts
@@ -2,10 +2,11 @@ import { invoke } from "@tauri-apps/api/core";
 import { emit, listen } from "@tauri-apps/api/event";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  const isTauri = typeof window !== "undefined" && "__TAURI__" in window;
+  const isTauri =
+    typeof window !== "undefined" && "__TAURI_INTERNALS__" in window;
   if (!isTauri) {
     console.warn("Not running Tauri:");
-    console.info(window);
+    console.log(window);
   }
 
   nuxtApp.provide("tauri", { invoke, listen, emit, isTauri });
