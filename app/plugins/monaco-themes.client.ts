707f01433049d52bb621eda31906a615c4d3c116
Kitsai <lucas.rocha.3801@gmail.com>
2026-03-21 22:35:15 -0300

    fix: fixed wrogn function call

--- app/plugins/monaco-themes.client.ts
+++ app/plugins/monaco-themes.client.ts
@@ -1,6 +1,10 @@
-import { convertVSCodeToMonaco, type ThemeDefinition, type VSCodeTheme } from '~/types/monaco/ThemeDefinition'
-import NightOwlDark from '~/assets/themes/Night-Owl-Dark.json'
-import NightOwlLight from '~/assets/themes/Night-Owl-Light.json'
+import {
+  convertVSCodeToMonaco,
+  type ThemeDefinition,
+  type VSCodeTheme,
+} from "~/types/monaco/ThemeDefinition";
+import NightOwlDark from "~/assets/themes/Night-Owl-Dark.json";
+import NightOwlLight from "~/assets/themes/Night-Owl-Light.json";
 
 export default defineNuxtPlugin(async () => {
   // SSR is false and we're in a .client.ts plugin, so this is always browser-only
@@ -10,8 +14,8 @@ export default defineNuxtPlugin(async () => {
     let owl_dark = convertVSCodeToMonaco(NightOwlDark as VSCodeTheme);
     let owl_light = convertVSCodeToMonaco(NightOwlLight as VSCodeTheme);
 
-    monaco.editor.defineTheme('night-owl-dark', owl_dark);
-    monaco.editor.defineTHeme('night-owl-light', owl_light);
+    monaco.editor.defineTheme("night-owl-dark", owl_dark);
+    monaco.editor.defineTheme("night-owl-light", owl_light);
 
     console.log("Monaco theme registration plugin initialized");
   } catch (error) {
